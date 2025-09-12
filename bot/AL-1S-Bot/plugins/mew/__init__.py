cc777b9cfcdf2af6f3b23833b9790f6a6a3d7569
Reyar <2290197845@qq.com>
2025-09-12 19:34:34 +0800

    feat Create Mew Plugin

---
+++ bot/AL-1S-Bot/plugins/mew/__init__.py
@@ -0,0 +1,27 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
+import requests
+
+__plugin_meta__ = PluginMetadata(
+    name="mew",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+mew = on_command("喵")
+
+@mew.handle()
+async def mew_handle(event: Event):
+    id = event.get_user_id()
+    if id == "2290197845":
+        await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 喵~"))
+    else:
+        await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 哈！"))
