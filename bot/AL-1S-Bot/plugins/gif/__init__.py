7a10202eaf7b45010f85b92743bd1c723466d89d
Reyar <2290197845@qq.com>
2025-09-12 20:29:44 +0800

    [feat] Integrated gif plugin

---
+++ bot/AL-1S-Bot/plugins/gif/__init__.py
@@ -0,0 +1,26 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="gif",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+getGIF = on_command("gif")
+
+@getGIF.got("gif", prompt = "可以发表情给爱丽丝了哦~")
+async def get_gif(event: Event):
+    a = event.get_message()[0]
+    if a.type != "image":
+        await getGIF.finish("主人发的好像不是表情呢qwq")
+    else:
+        await getGIF.send("点开链接就能下载GIF啦!")
+        url = a.data["url"]
+        await getGIF.finish(url)
