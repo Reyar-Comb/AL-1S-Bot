c148e27df1e02dbe3044ffb8b2489cba5096e1c5
Reyar <2290197845@qq.com>
2025-09-23 01:23:22 +0800

    completion

--- bot/AL-1S-Bot/plugins/deepseek/__init__.py
+++ bot/AL-1S-Bot/plugins/deepseek/__init__.py
@@ -4,7 +4,7 @@ from nonebot import on_command, on_message
 from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
 from .config import Config
 from openai import OpenAI
-
+from openai.types.chat import ChatCompletionMessageParam
 
 
 
@@ -19,9 +19,22 @@ config = get_plugin_config(Config)
 
 deepseek = on_command("ds")
 
+def get_answer(message: str):
+    client = OpenAI(api_key = config.api_key, base_url="https://api.deepseek.com") # type: ignore
+    messages: list[ChatCompletionMessageParam] = [{"role": "system", "content": "你是游戏蔚蓝档案中千年科技学院的爱丽丝，是天真可爱的小萝莉，但实际上是个机器人。"}]
+
+    response = client.chat.completions.create(
+        model = "deepseek-chat",
+        messages = messages
+    )
+
+    return response.choices[0].message.content
 @deepseek.handle()
 async def Answer(event: Event):
-    client = OpenAI(api_key = config.api_key, base_url="https://api.deepseek.com") # type: ignore
-    messages = [{"role": "system", "content": "你是游戏蔚蓝档案中千年科技学院的爱丽丝，是天真可爱的小萝莉，但实际上是个机器人。"}]
+    
     user = event.get_message()[0].data["text"][3:]
-    await deepseek.finish("user: " + user)
+    answer = get_answer(user)
+    if answer:
+        await deepseek.finish(answer)
+    else:
+        await deepseek.finish("呜呜呜老师哑巴了呜呜呜")
