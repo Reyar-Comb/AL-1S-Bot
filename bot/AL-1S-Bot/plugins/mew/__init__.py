0fea5c77a86a7bfaf14c0460e8f62ff89d4e47c2
Reyar <2290197845@qq.com>
2025-10-02 01:09:04 +0800

    Edit miao

--- bot/AL-1S-Bot/plugins/mew/__init__.py
+++ bot/AL-1S-Bot/plugins/mew/__init__.py
@@ -2,7 +2,7 @@ from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
-
+import random
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
 import requests
@@ -23,7 +23,17 @@ mew = on_command("喵")
 async def mew_handle(event: Event):
     id = event.get_user_id()
     time.sleep(2)
-    if id == "2290197845":
+    if id == config.admin_id:  # type: ignore
         await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 喵~"))
+
+    elif id == config.friend_id:  # type: ignore
+        await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 喵呜~"))
+
     else:
-        await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 哈!"))
+        t = random.randint(1, 10)
+        if t <= 6:
+            await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 喵!"))
+        elif t <= 9:
+            await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 喵喵!"))
+        else:
+            await mew.finish(MessageSegment.at(id) + MessageSegment.text(" 哈!"))
